6da2ed740dbfe893ae4ce3878d7027e3dcc7af62
michal.kajstura <michal.kajstura@alphamoon.ai>
2020-08-15 13:02:37 +0200

    refactor: Move DEFAULT_ESTIMATOR to settings

--- active_labeling/scripts/run_api.py
+++ active_labeling/scripts/run_api.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 from active_labeling.backend.api import ActiveLearning
+from active_labeling.settings import DEFAULT_ESTIMATOR
 
 if __name__ == '__main__':
     path = Path('/home/michal/projects/thesis/mnist-csv-png/test')
-    active_learning = ActiveLearning(path)
+    active_learning = ActiveLearning(path, DEFAULT_ESTIMATOR())
     active_learning.run()
--- active_labeling/settings.py
+++ active_labeling/settings.py
@@ -1,1 +1,4 @@
+from sklearn.ensemble import RandomForestClassifier
+
 DEFAULT_BATCH_SIZE = 10
+DEFAULT_ESTIMATOR = RandomForestClassifier
