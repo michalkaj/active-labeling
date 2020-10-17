6a3f4a9aed3b404f57779cb460b089d699356fe3
michal.kajstura <michal.kajstura@alphamoon.ai>
2020-10-17 17:10:10 +0200

    feat: Add ActiveDataset

--- active_labeling/backend/resources/metrics.py
+++ active_labeling/backend/resources/metrics.py
@@ -30,7 +30,7 @@ class Metrics(Resource):
 
     def _get_label_frequencies(self) -> Dict[str, int]:
         counts = {label: 0 for label in self._config.labels}
-        reverse_mapping = {i: label for label, i in self._active_dataset.label_mapping().items()}
+        reverse_mapping = {i: label for label, i in self._active_dataset.label_mapping.items()}
         for label_idx in self._active_dataset.labels.values():
             label = reverse_mapping[label_idx]
             counts[label] += 1
